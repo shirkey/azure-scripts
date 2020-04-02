3a054463ff5ad09be56b320f913b0ccf9fee2aa5
shirkey <shirkey@users.noreply.github.com>
2020-04-02 11:57:25 +0800

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
+
+# execute this test script directly from Github via:
+# wget -O - https://raw.githubusercontent.com/shirkey/azure-scripts/master/echo.sh | bash
+
+# this can be executed from the bash shell in the Azure portal (at portal.azure.com)
+# or (even easier) from https://shell.azure.com, selecting Bash from the shell option in the header bar
+
 echo "Script successfully executed"
