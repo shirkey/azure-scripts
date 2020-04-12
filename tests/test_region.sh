d94ec6fa6d7753e1d2871878087046c0efdb3ffe
Shirkey <shirkeydev@gmail.com>
2020-04-12 07:08:21 +0000

    adding initial tests

---
+++ tests/test_region.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+source_file="get_region.sh"
+
+source ${source_file}
+
+echo "Testing: ${source_file}"
+echo "Test function: select_region"
+select_region
---
+++ tests/test_subscription.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+source_file="get_subscription.sh"
+
+source ${source_file}
+
+echo "Testing: ${source_file}"
+echo "Test function: select_subscription"
+select_subscription
