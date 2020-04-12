37a7f702aaa6c887ed68e190bf1e2744a1906853
Shirkey <shirkeydev@gmail.com>
2020-04-12 06:54:53 +0000

    Adding local cache

--- get_region.sh
+++ get_region.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
-IFS=","
-declare -a sub_names
+declare -a region_names
+declare -a region_ids
+
+declare region_file="/tmp/regions.json"
+declare selected_region
 
 function get_regions()
 {
-region_names=($(az account list-locations | jq -s '[ .[][].displayName ] | join(",")'  | tr -d "\"" ))
-region_ids=($(az account list-locations | jq -s '[ .[][].name ] | join(",")'  | tr -d "\"" ))
+IFS=","
+if [ ! -f "${region_file}" ]; then
+    echo "Retrieving regions, caching to ${region_file}"
+    $(az account list-locations > ${region_file})
+else
+    echo "Retrieving regions from cache (${region_file})"
+fi
+region_names=($(jq -s '[ .[][].displayName ] | join(",")' ${region_file} | tr -d "\""))
+region_ids=($(jq -s '[ .[][].name ] | join(",")' ${region_file} | tr -d "\""))
 }
 
 function select_region() 
@@ -14,11 +24,11 @@ PS3="Select region: "
 get_regions
 select region_name in ${region_names[@]}
 do 
-region=${region_ids[${REPLY}-1]}
-echo "Selected: ${region_name} (Region: ${region})"
+selected_region=${region_ids[${REPLY}-1]}
+echo "You selected: ${region_name} (Region: ${selected_region})"
 break
 done
 }
 
-select_region
-echo  ${region}
+#select_region
+#echo  ${selected_region}
