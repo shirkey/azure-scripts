a0f23903b9b5a614cb6cfe638b3d0d5103633f2e
Shirkey <shirkeydev@gmail.com>
2020-04-12 07:07:03 +0000

    added caching

--- get_subscription.sh
+++ get_subscription.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 IFS=","
 declare -a sub_names
+declare -a sub_ids
+
+declare subscription_file="/tmp/subscriptions.json"
+declare selected_subscription
 
 function get_subscriptions()
 {
-sub_names=($(az account list | jq -s '[ .[][].name ] | join(",")'  | tr -d "\"" ))
-sub_ids=($(az account list | jq -s '[ .[][].id ] | join(",")'  | tr -d "\"" ))
+IFS=","
+if [ ! -f "${subscription_file}" ]; then
+    echo "Retrieving subscriptions, caching to ${subscription_file}"
+    $(az account list > ${subscription_file})
+else
+    echo "Retrieving subscriptions from cache (${subscription_file})"
+fi
+sub_names=($(jq -s '[ .[][].name ] | join(",")' ${subscription_file} | tr -d "\"" ))
+sub_ids=($(jq -s '[ .[][].id ] | join(",")' ${subscription_file} | tr -d "\"" ))
 }
 
 function select_subscription() 
@@ -14,11 +25,8 @@ PS3="Select subscription: "
 get_subscriptions
 select sub_name in ${sub_names[@]}
 do 
-subscription=${sub_ids[${REPLY}-1]}
-echo "Selected: ${sub_name} (Subscription: ${subscription})"
+selected_subscription=${sub_ids[${REPLY}-1]}
+echo "Selected: ${sub_name} (Subscription: ${selected_subscription})"
 break
 done
 }
-
-select_subscription
-echo  ${subscription}
