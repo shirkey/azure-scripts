9054f5adbf6900f869b61948dec2d514cb08d8c5
Shirkey <shirkeydev@gmail.com>
2020-04-13 13:25:10 +0000

    changed compilation steps, recompiled

--- make
+++ make
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
-# remove previous run.sh
+# remove previous run.sh, initialize
 rm ./run.sh
+echo "#!/usr/bin/env bash" > ./run.sh
 
-# copy src/main.sh as baseline run.sh
-cp ./src/main.sh ./run.sh
-
-# append src/get_* functions to new run.sh
+# append src/get_* functions to run.sh
 for f in ./src/get_*; do tail -n +2 "$f" >> ./run.sh; done
+
+# append src/main.sh to run.sh
+tail -n +5 ./src/main.sh >> ./run.sh
 
 # make executable for testing
 chmod +x ./run.sh
--- run.sh
+++ run.sh
@@ -1,41 +1,4 @@
 #!/usr/bin/env bash
-source src/get_subscription.sh
-source src/get_region.sh
-source src/get_usage.sh
-
-slug="*************************************"
-header=$"${slug}\n     Azure Usage Checker\n${slug}\n"
-
-clear
-echo -e ${header}
-select_subscription
-
-header+=" - Subscription: ${selected_subscription}\n"
-clear
-echo -e ${header}
-select_region
-header+=" - Region: ${selected_region}\n"
-
-while :
-do
-	clear
-	echo -e $header
-	echo ""
-	echo "1) Show VM usage"
-	echo "2) Quit"
-	echo ""
-	read -p "Enter option: " opt
-
-	case $opt in
-	1)
-		show_vm_usage_by_region ${selected_region}
-		read -p "Press [Enter] key to continue..." readEnterKey
-		;;
-	2)
-		echo "Exiting..."
-		exit 0
-	esac
-done
 declare -a region_names
 declare -a region_ids
 
@@ -215,3 +178,37 @@ function show_vm_usage_by_region()
 		printf "%50b: %3b%% (%4b of %4b)\e[0m\n" $vmname $vmusagepercent ${vmusage} ${vmlimit}
 	done
 }
+
+slug="*************************************"
+header=$"${slug}\n     Azure Usage Checker\n${slug}\n"
+
+clear
+echo -e ${header}
+select_subscription
+
+header+=" - Subscription: ${selected_subscription}\n"
+clear
+echo -e ${header}
+select_region
+header+=" - Region: ${selected_region}\n"
+
+while :
+do
+	clear
+	echo -e $header
+	echo ""
+	echo "1) Show VM usage"
+	echo "2) Quit"
+	echo ""
+	read -p "Enter option: " opt
+
+	case $opt in
+	1)
+		show_vm_usage_by_region ${selected_region}
+		read -p "Press [Enter] key to continue..." readEnterKey
+		;;
+	2)
+		echo "Exiting..."
+		exit 0
+	esac
+done
